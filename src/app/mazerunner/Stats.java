e80169b0bb47f46564037133e07762b2624b7488
Jayen <jn_jaentilal@hotmail.co.uk>
2013-02-21 16:09:04 +0000

    Finished stats layout

--- src/app/mazerunner/Stats.java
+++ src/app/mazerunner/Stats.java
@@ -28,5 +28,23 @@ public class Stats extends Activity {
 		
 		bonus=(TextView) findViewById(R.id.statsBonusTV);
 		bonus.setText(getString(R.string.bonus)+data.getInt(Game.BONUS,0));
+		
+		highestScore=(TextView) findViewById(R.id.highestScoreTV);
+		highestScore.setText(getString(R.string.highScore)+" "+data.getInt(Game.SCORE_HIGHEST,0));
+		
+		mostCoins=(TextView) findViewById(R.id.mostCoinsTV);
+		mostCoins.setText(getString(R.string.mostCoins)+" "+data.getInt(Game.COINS_HIGHEST,0));
+		
+		highestDistance=(TextView) findViewById(R.id.highestDistanceTV);
+		highestDistance.setText(getString(R.string.highestDistance)+"  "+data.getFloat(Game.DISTANCE_HIGHEST,0));
+		
+		totalGames=(TextView) findViewById(R.id.totalGamesTV);
+		totalGames.setText(getString(R.string.totalGames)+"  "+data.getInt(Game.NUMBEROFGAMESPLAYED,0));
+		
+		totalDistance=(TextView) findViewById(R.id.totalDistanceTV);
+		totalDistance.setText(getString(R.string.totalDistance)+" "+data.getFloat(Game.DISTANCE_OVERALL,0));
+		
+		totalCoins=(TextView) findViewById(R.id.totalCoinsTV);
+		totalCoins.setText(getString(R.string.totalCoins)+"  "+data.getInt(Game.COINS_OVERALL,0));
 	}
 }
