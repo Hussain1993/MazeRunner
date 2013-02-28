308aa3e7078700de91b4c9c2878a198f7af5cd58
Jayen <jn_jaentilal@hotmail.co.uk>
2013-02-28 13:21:16 +0000

    change main activity

--- src/app/mazerunner/MainActivity.java
+++ src/app/mazerunner/MainActivity.java
@@ -7,6 +7,10 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
+import android.media.SoundPool.OnLoadCompleteListener;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -17,7 +21,10 @@ import android.widget.Button;
  * @author Jayen
  */
 public class MainActivity extends Activity {
-
+	private SoundPool soundPool;
+	private int soundId;
+	private boolean loaded = false;
+	
 	private static final int START_GAME_DIALOG = 0;
 	private static Boolean first;
 	private SharedPreferences prefs;
@@ -29,19 +36,43 @@ public class MainActivity extends Activity {
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    	System.out.println("HElLO WORLD");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         prefs = this.getSharedPreferences("app.mazerunner", Context.MODE_PRIVATE);
         final String booleanKey = "com.mazerunner.booleanKey";
         first = prefs.getBoolean(booleanKey, true);
-        
+//        MediaPlayer player = MediaPlayer.create(this, R.raw.music);
+//        player.start();
+//        player.setLooping(true); // Set looping
+//        player.setVolume(100,100);
+//        this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
+//        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+//        soundPool.setOnLoadCompleteListener(new OnLoadCompleteListener() {
+//			
+//			@Override
+//			public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+//				loaded = true;
+//			}
+//		});
+//        soundId = soundPool.load(this, R.raw.music, 1);
+//        AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+//        float actualVolume = (float) audioManager
+//            .getStreamVolume(AudioManager.STREAM_MUSIC);
+//        float maxVolume = (float) audioManager
+//            .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+//        float volume = actualVolume / maxVolume;
+//        if(loaded)
+//        {
+//        	soundPool.play(soundId, volume, volume, 1, 0, 1f);
+//        	soundPool.setLoop(soundId, -1);
+//        }
         //setup startGameButton
         //===============================================================================================
         startGameButton=(Button) findViewById(R.id.Game);//find the startGame button by id
         //add the onClickListener for the startGame
         startGameButton.setOnClickListener(new OnClickListener() {
 			
+			@SuppressWarnings("deprecation")
 			@Override
 			public void onClick(View v) {
 				if(prefs.getBoolean(booleanKey, true) == true)
@@ -73,28 +104,28 @@ public class MainActivity extends Activity {
 
         //setup storeButton
         //=================================================================================================
-        storeButton=(Button) findViewById(R.id.Store);//find the startGame button by id
-        //add the onClickListener for the startGame
-        storeButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				startActivity(new Intent(getBaseContext(),Store.class));
-			}
-		});
+//        storeButton=(Button) findViewById(R.id.Store);//find the startGame button by id
+//        //add the onClickListener for the startGame
+//        storeButton.setOnClickListener(new OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				startActivity(new Intent(getBaseContext(),Store.class));
+//			}
+//		});
         //=================================================================================================
         
         //setup optionsButton
         //=================================================================================================
-        achievementsButton=(Button) findViewById(R.id.achievements);//find the startGame button by id
-        //add the onClickListener for the startGame
-        achievementsButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				startActivity(new Intent(getBaseContext(), Achievements.class));
-			}
-		});
+//        achievementsButton=(Button) findViewById(R.id.achievements);//find the startGame button by id
+//        //add the onClickListener for the startGame
+//        achievementsButton.setOnClickListener(new OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				startActivity(new Intent(getBaseContext(), Achievements.class));
+//			}
+//		});
         //=================================================================================================
         
         //setup exitButton
